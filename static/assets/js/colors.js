f90e44243e3bfe18bda06b893ebc01c4a1be9fcd
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-25 15:20:05 +0300

    Fix code - I was sleep deprived, don't judge :(

--- static/assets/js/colors.js
+++ static/assets/js/colors.js
@@ -13,42 +13,30 @@ document.documentElement.setCookie = function(name, value){
 }
 
 // Keep track of the current Docusaurus theme!
-const getThemeSwitch = () => document.querySelector('.navbar__items--right button'); // Select the theme switch element
 let themeSwitch = undefined;
+const getThemeSwitch = () => (themeSwitch || (themeSwitch = document.querySelector('.navbar__items--right button'))); // Select the theme switch element
+const isDark = () => document.documentElement.dataset.theme == "dark"; // Check the current theme!
 function toggleDocusaurusTheme(){
-    if (themeSwitch) {
+    if (getThemeSwitch()) {
         // Trigger a click event on the theme switch
-        themeSwitch.click();
-        themeSwitch.isDark = !themeSwitch.isDark;
+        getThemeSwitch().click();
     }
 }
 
 // Handle theme!
 window.indepColorSchemeUpdate = (forced, dark) => {
-    console.log(forced, dark);
-    // let trackedClasses = [document.documentElement.classList, document.body.classList];
     if(dark){
-        if(!themeSwitch.isDark){
+        if(!isDark()){
             toggleDocusaurusTheme();
         }
-        // document.documentElement.dataset.theme = "dark";
-        // trackedClasses.map((e) => { e.remove("light"); e.add("dark"); });
     }else{
-        if(themeSwitch.isDark){
+        if(isDark()){
             toggleDocusaurusTheme();
         }
-        // document.documentElement.dataset.theme = "light";
-        // trackedClasses.map((e) => { e.remove("dark"); e.add("light"); });
     }
 };
 
 // Wait for the page to finish loading!
 document.addEventListener('docusaurus-plugin-content-docs-refresh', (event) => {
-    let interval = setInterval(() => {
-        if((themeSwitch = getThemeSwitch()) != undefined){
-            clearInterval(interval);
-            themeSwitch.isDark = (document.documentElement.dataset.theme == "dark");
-            setTimeout(() => document.documentElement.indepColors = true, 100);
-        }
-    }, 10);
+    document.documentElement.indepColors = true;
 });
