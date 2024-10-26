fedc394b5282cd9a73136fc4fe9912df0db51aab
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-26 22:34:22 +0300

    Remove unused code

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,58 +1,19 @@
 import React, { useEffect } from 'react';
-import clsx from 'clsx';
-import ErrorBoundary from '@docusaurus/ErrorBoundary';
-import {
-  PageMetadata,
-  SkipToContentFallbackId,
-  ThemeClassNames,
-} from '@docusaurus/theme-common';
-import {useKeyboardNavigation} from '@docusaurus/theme-common/internal';
-import SkipToContent from '@theme/SkipToContent';
-import AnnouncementBar from '@theme/AnnouncementBar';
-import Navbar from '@theme/Navbar';
-import LayoutProvider from '@theme/Layout/Provider';
-import ErrorPageContent from '@theme/ErrorPageContent';
-// Refresh event!
-export default function Layout(props) {
-  const {
-    children,
-    wrapperClassName,
-    // Not really layout-related, but kept for convenience/retro-compatibility
-    title,
-    description,
-  } = props;
-  useKeyboardNavigation();
+import Layout from '@theme-original/Layout';
+
+export default function LayoutWrapper(props) {
   useEffect(() => {
     // Dispatch the event when the component mounts
-    setTimeout(function(){
-      const myEvent = new CustomEvent('docusaurus-plugin-content-docs-refresh', { detail: { message: true } });
-      document.dispatchEvent(myEvent);
-    }, 0);
+    const refreshEvent = new CustomEvent('docusaurus-plugin-content-docs-refresh', { detail: { message: true } });
+    document.dispatchEvent(refreshEvent);
   }, []);
   return (
-    <LayoutProvider>
-      <PageMetadata title={title} description={description} />
-
-      <SkipToContent />
-
-      <AnnouncementBar />
-
-      <Navbar />
-
-      <x-layout
-        id={SkipToContentFallbackId}
-        className={clsx(
-          ThemeClassNames.wrapper.main,
-          wrapperClassName,
-        )}>
-        <div slot="group">
-          <div view="full" class="x-layout-pane">
-            <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
-              {children}
-            </ErrorBoundary>
-            </div>
+    <x-layout>
+      <div slot="group">
+        <div view="full" class="x-layout-pane">
+          <Layout {...props} />
         </div>
-      </x-layout>
-    </LayoutProvider>
+      </div>
+    </x-layout>
   );
 }
