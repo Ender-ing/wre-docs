1532ec32120cc1db3830bc44dd22fea349beae77
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-27 01:36:33 +0300

    Enable the use of `XLayout` elements within documents

---
+++ src/theme/DocRoot/Layout/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {useDocsSidebar} from '@docusaurus/plugin-content-docs/client';
+import BackToTopButton from '@theme/BackToTopButton';
+import DocRootLayoutSidebar from '@theme/DocRoot/Layout/Sidebar';
+import DocRootLayoutMain from '@theme/DocRoot/Layout/Main';
+import styles from './styles.module.css';
+import XLayout, { XLayoutGroup, XLayoutPane } from '../../XLayout';
+export default function DocRootLayout({children}) {
+  const sidebar = useDocsSidebar();
+  const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
+  return (
+    <div className={styles.docsWrapper}>
+      <BackToTopButton />
+      <XLayout className={styles.docRoot}>
+        {sidebar && (
+          <XLayoutGroup view="static sticky disappear">
+            <XLayoutPane view="full">
+              <DocRootLayoutSidebar
+                sidebar={sidebar.items}
+                hiddenSidebarContainer={hiddenSidebarContainer}
+                setHiddenSidebarContainer={setHiddenSidebarContainer}
+              />
+            </XLayoutPane>
+          </XLayoutGroup>
+        )}
+        <XLayoutGroup>
+          <XLayoutPane view="full">
+            <DocRootLayoutMain hiddenSidebarContainer={hiddenSidebarContainer}>
+              {children}
+            </DocRootLayoutMain>
+          </XLayoutPane>
+        </XLayoutGroup>
+      </XLayout>
+    </div>
+  );
+}
