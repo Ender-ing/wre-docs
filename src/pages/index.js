e6f1c358e81402ce7c97a57a8f3f6c8c9a9be596
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-27 01:36:44 +0300

    Enable the use of `XLayout` elements in the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import Head from '@docusaurus/Head';
+import XLayout, { XLayoutGroup, XLayoutPane } from '../theme/XLayout';
 
 export default function Home() {
     const { siteConfig } = useDocusaurusContext();
@@ -13,7 +14,6 @@ export default function Home() {
         <Layout
             title={`${siteConfig.title}`}
             description="Documentation for all Ender.ing projects">
-
 
             <Head>
                 <html lang="en" dir="ltr" translate="no" />
@@ -23,12 +23,19 @@ export default function Home() {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link rel="preconnect" href="https://www.googletagmanager.com" crossorigin />
             </Head>
-            <h1>This is the temporary main docs page!</h1>
-            <br/>
-            <md-elevated-button href="/docs/old/intro">Old Docs</md-elevated-button> <br/>
-            <md-elevated-button href="/docs/contribution/intro">Contribution Docs</md-elevated-button> <br/>
-            <md-elevated-button href="/docs/omni-std1/intro">Omniarium Standard Edition 1.0 Docs</md-elevated-button> <br/>
-            <md-elevated-button href="/blog">Blog</md-elevated-button> <br/>
+
+            <XLayout>
+                <XLayoutGroup>
+                    <XLayoutPane view="full">
+                        <h1>This is the temporary main docs page!</h1>
+                        <br/>
+                        <md-elevated-button href="/docs/old/intro">Old Docs</md-elevated-button> <br/>
+                        <md-elevated-button href="/docs/contribution/intro">Contribution Docs</md-elevated-button> <br/>
+                        <md-elevated-button href="/docs/omni-std1/intro">Omniarium Standard Edition 1.0 Docs</md-elevated-button> <br/>
+                        <md-elevated-button href="/blog">Blog</md-elevated-button> <br/>
+                    </XLayoutPane>
+                </XLayoutGroup>
+            </XLayout>
         </Layout>
     );
 }
