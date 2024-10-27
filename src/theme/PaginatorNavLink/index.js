a05df39756f184ef4c29f08b1d59f582d3b73f56
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-27 16:37:09 +0200

    Fix nav links

--- src/theme/PaginatorNavLink/index.js
+++ src/theme/PaginatorNavLink/index.js
@@ -4,7 +4,10 @@ import clsx from 'clsx';
 
 export default function PaginatorNavLinkWrapper(props) {
   return (
-    <div className={clsx("page-navigation-wrapper", (props.isNext) ? "pagination-nav__link--next next-navigation-wrapper-fix" : "")}>
+    <div className={clsx("page-navigation-wrapper",
+          (props.isNext) ? "pagination-nav__link--next next-navigation-wrapper-fix" :
+            "pagination-nav__link--prev prev-navigation-wrapper-fix"
+        )}>
       <md-ripple aria-hidden="true"></md-ripple>
       <PaginatorNavLink {...props} />
     </div>
