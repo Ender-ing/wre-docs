556a35b726e14a426ffdbcfaa316811831d76b9f
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-10-27 20:59:25 +0200

    Update the `PaginatorNavLink` element

--- src/theme/PaginatorNavLink/index.js
+++ src/theme/PaginatorNavLink/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import PaginatorNavLink from '@theme-original/PaginatorNavLink';
 import clsx from 'clsx';
-
-export default function PaginatorNavLinkWrapper(props) {
+import Link from '@docusaurus/Link';
+export default function PaginatorNavLink(props) {
+  const {permalink, title, subLabel, isNext} = props;
   return (
-    <div className={clsx("page-navigation-wrapper",
-          (props.isNext) ? "pagination-nav__link--next next-navigation-wrapper-fix" :
-            "pagination-nav__link--prev prev-navigation-wrapper-fix"
-        )}>
-      <md-ripple aria-hidden="true"></md-ripple>
-      <PaginatorNavLink {...props} />
-    </div>
+    <x-effect
+      list="ripple elevation focus"
+      class={clsx("page-navigation-wrapper",
+      (props.isNext) ? "pagination-nav__link--next next-navigation-wrapper-fix" :
+        "pagination-nav__link--prev prev-navigation-wrapper-fix"
+      )}>
+      <Link
+        slot="child"
+        className={clsx(
+          'pagination-nav__link',
+          isNext ? 'pagination-nav__link--next' : 'pagination-nav__link--prev',
+        )}
+        to={permalink}>
+        {subLabel && <div className="pagination-nav__sublabel">{subLabel}</div>}
+        <div className="pagination-nav__label">{title}</div>
+      </Link>
+    </x-effect>
   );
 }
