2f8aba940c981e76dc14a7f2a345570f1121cf09
0xENDER <72353925+0xENDER@users.noreply.github.com>
2024-11-08 14:24:58 +0200

    Update docs blog

---
+++ src/theme/BlogTagsListPage/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import clsx from 'clsx';
+import {
+  PageMetadata,
+  HtmlClassNameProvider,
+  ThemeClassNames,
+  translateTagsPageTitle,
+} from '@docusaurus/theme-common';
+import BlogLayout from '@theme/BlogLayout';
+import TagsListByLetter from '@theme/TagsListByLetter';
+import SearchMetadata from '@theme/SearchMetadata';
+import Heading from '@theme/Heading';
+export default function BlogTagsListPage({tags, sidebar}) {
+  const title = translateTagsPageTitle();
+  return (
+    <HtmlClassNameProvider
+      className={clsx(
+        ThemeClassNames.wrapper.blogPages,
+        ThemeClassNames.page.blogTagsListPage,
+      )}>
+      <PageMetadata title={title} />
+      <SearchMetadata tag="blog_tags_list" />
+      <BlogLayout sidebar={sidebar}>
+        <div class="x-layout-pane" view="full">
+          <Heading as="h1">{title}</Heading>
+          <TagsListByLetter tags={tags} />
+        </div>
+      </BlogLayout>
+    </HtmlClassNameProvider>
+  );
+}
